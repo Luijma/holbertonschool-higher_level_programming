56ffe3df945349aaff7f384b2c6464102117327b
Luijma <rsluismanuel@gmail.com>
2021-09-08 02:58:59 +0000

    T8 A1

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, oject-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:65] + " " + "with Python"
+print(str)
