03dc86746755844ce63e99ab9b3dfd8ac8157526
Luijma <rsluismanuel@gmail.com>
2021-09-08 20:41:06 +0000

    T1 A1, I expect a failure due to abs() function

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+digit = abs(number) % 10
+print("Last digit of {} is {}".format(number, digit), end=" ")
+if digit > 5:
+    print("and is greater than 5")
+elif digit == 0:
+    print("and is 0")
+elif digit < 6:
+    print("and is less than 6 and not 0")
