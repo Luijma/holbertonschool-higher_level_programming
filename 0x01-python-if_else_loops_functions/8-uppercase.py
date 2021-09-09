9a788062d4af07afbf7deaf6fc0b7129e8a230c4
Luijma <rsluismanuel@gmail.com>
2021-09-09 01:03:17 +0000

    T8 A2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/pyton3
 def uppercase(str):
-    capword = str
     for i in str:
-        if ord(i) > 96 and ord(i) < 123:
-            capword = capword.replace(i, chr(ord(i) - 32))
-    print(capword)
+        x = ord(i)
+        if x > 96 and x < 123:
+            x -= 32
+        print(chr(x), end='')
+    print('')
