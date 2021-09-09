afe8863ae035db7b9b8af320148363fe49bc837d
Luijma <rsluismanuel@gmail.com>
2021-09-09 03:24:28 +0000

    I fixed the while and got rid of unnecessary if

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -23,12 +23,10 @@ listint_t *insert_node(listint_t **head, int number)
 		return (newnode);
 	}
 
-	while (current && current->next)
+	while ((current && current->next) && (current->next->n < newnode->n))
 	{
 		if (current->next->n < number)
 			current = current->next;
-		else
-			break;
 	}
 	newnode->next = current->next;
 	current->next = newnode;
