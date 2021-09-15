12bbec1e3a809733be6b77ab95ed81883dc2bd88
Luijma <rsluismanuel@gmail.com>
2021-09-15 03:56:54 +0000

    T4 A3

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
     if my_list != None:
-        new_list = my_list[:]
         if idx >= 0 or idx < len(my_list):
             new_list[idx] = element
-            return new_list
-    return None
+    return new_list
