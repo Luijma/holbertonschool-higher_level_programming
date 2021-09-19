b8d1700666e447c644588e627e4bf663183dd403
Luijma <rsluismanuel@gmail.com>
2021-09-19 22:47:21 +0000

    T5 A1

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     noC = ""
     for current in my_string:
         if current != "C" and current != "c":
-            current = current + my_string
+            current += my_string
     return noC
