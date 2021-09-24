b1f2631b82051b99c1e698686064a2b80e9573d9
Luijma <rsluismanuel@gmail.com>
2021-09-24 15:33:09 +0000

    t13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -65,3 +65,32 @@ void turn_around(listint_t **head)
 	}
 	*head = slow;
 }
+/**
+ * compare_lists - compares list for reversal
+ * @head_1: first head
+ * @head_2: second head
+ * Return: 1 if the same, 0 if not
+ */
+int compare_lists(listint_t *head_1, listint_t *head_2)
+{
+	listint_t *temp_1 = head_1;
+	listint_t *temp_2 = head_2;
+
+	while (temp_1 && temp_2)
+	{
+		if (temp_1->n == temp_2->n)
+		{
+			temp_1 = temp_1->next;
+			temp_2 = temp_2->next;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	if (!temp_1 && !temp_2)
+	{
+		return (1);
+	}
+	return (0);
+}
