cbd4e64fd01b9c17173a0a03603b3f691badec3f
Luijma <rsluismanuel@gmail.com>
2022-01-03 22:17:48 +0000

    Fixed style

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix != None:
+    if matrix is not None:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 print("{:d}".format(matrix[i][j]), end="")
