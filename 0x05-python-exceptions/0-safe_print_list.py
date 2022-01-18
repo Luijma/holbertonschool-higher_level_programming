389c93e622fb4f54541c6e7f04e05a86dbb04dce
Luijma <rsluismanuel@gmail.com>
2022-01-18 18:04:25 +0000

    Simplified code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             i += 1
     except IndexError:
-        print("")
-    else:
-        print("")
+        pass
+    print("")
     return i
