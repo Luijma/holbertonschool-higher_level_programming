a319d52d94b76fd38ffb5369cd4b2cf19aba4345
Luijma <rsluismanuel@gmail.com>
2022-01-18 19:40:18 +0000

    T1 A2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format())
-    except Value:
+        print("{:d}".format(value))
+    except BaseException:
         return False
     else:
         return True
