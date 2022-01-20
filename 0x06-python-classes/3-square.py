4282a850fe8dfb9d1bb7bd3947c1f5cf98691255
Luijma <rsluismanuel@gmail.com>
2022-01-20 00:42:50 +0000

    T3 A1

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module for the square class """
+
+
+class Square:
+    """ Square class description """
+    __size = None
+
+    def __init__(self, size=0):
+        """ docstring of __init__ method
+        Args:
+            size (int): size passed to constructor."""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        """ int: attribute with type specified"""
+
+    def area(self):
+        """ docstring of area method. """
+        return self.__size ** 2
+    """ returns the size attribute with value """
