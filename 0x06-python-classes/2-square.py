ab75dc87a5fa82ab8c88e091168b2d7dbc7f6b9c
Luijma <rsluismanuel@gmail.com>
2022-01-20 00:38:39 +0000

    T2 A2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,7 +12,7 @@ class Square:
             size (int): size passed to constructor."""
         if type(size) != int:
             raise TypeError("size must be an integer")
-        elif type(size) < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
         """ int: attribute with type specified"""
