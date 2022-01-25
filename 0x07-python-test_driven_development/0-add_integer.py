5cf9e5d462248857bb2bd7e2411cdf49b0dabbbf
Luijma <rsluismanuel@gmail.com>
2022-01-25 02:43:49 +0000

    T- A1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 
+    My addition module
+"""
+def add_integer(a, b=98):
+    """ Addition function """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    elif type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
