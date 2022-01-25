7732ff46719f7e7ade0e9e7e65150c894d8fd6df
Luijma <rsluismanuel@gmail.com>
2022-01-25 19:31:22 +0000

    Adding most updated task 2 versions

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,7 +9,9 @@ def matrix_divided(matrix, div):
         div (Union[int, float]): number to be divided
     """
     is_correct_type = False
+    row_length = len(matrix[0])
     new_matrix = [i[:] for i in matrix]
+    
     if all(isinstance(ele, list) for ele in new_matrix) is True:
         is_correct_type = True
         for row in range(len(new_matrix)):
@@ -20,9 +22,18 @@ def matrix_divided(matrix, div):
                     break
             if (is_correct_type is False):
                 break
-    if(is_correct_type is False):
+    if (is_correct_type is False):
         raise TypeError("matrix must be a matrix (list of lists)" +
                         " of integers/floats")
+    for row in new_matrix:
+        if (len(row) != row_length):
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if (type(div) != int):
+        raise TypeError("div must be a number")
+    if (div == 0):
+        raise ZeroDivisionError("division by zero")
+
     new_matrix = [[round(i / div, 2) for i in j] for j in new_matrix]
     return new_matrix
     """ returns a new matrix with new values """
