6438a8f55bc64f8b918e07e056c6a77d8ec20187
Luijma <rsluismanuel@gmail.com>
2022-02-02 02:48:48 +0000

    T3 A1

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module for json string represenations
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ returns json representation
+    """
+
+    return json.dumps(my_obj)
