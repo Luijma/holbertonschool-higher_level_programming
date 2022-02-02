bdf99b9da8b22cbaf953e7a5f46355e35f9936aa
Luijma <rsluismanuel@gmail.com>
2022-02-02 02:43:23 +0000

    t2 A1

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Append to file module
+"""
+
+def append_write(filename="", text=""):
+    """ appends to textfile and returns written bytes """
+    with open(filename, mode='a', encoding='utf-8') as a_file:
+        return a_file.write(text)
