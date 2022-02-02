57d46d26804eacb280fa039e898f87ca9ac146c6
Luijma <rsluismanuel@gmail.com>
2022-02-02 02:28:08 +0000

    T0 A2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,5 @@ def read_file(filename=""):
     """ reads file and prints to screen """
 
     with open(filename, encoding='utf-8') as a_file:
-        print("{}".format(a_file.read()))
+        a_file.seek(0)
+        print("{}".format(a_file.read()), end="")
