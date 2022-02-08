55246a1119a01ee190af2d2e622f9d2e3b9e06c6
Luijma <rsluismanuel@gmail.com>
2022-02-08 01:03:31 +0000

    fixed typo

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -47,7 +47,7 @@ class Rectangle(Base):
         if (type(value) != int):
             raise TypeError("height must be an integer")
         if (value <= 0):
-            raise ValueError("width must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
