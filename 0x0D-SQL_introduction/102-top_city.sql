545ef882c2c18bbeceafdc884e8f0267abf56832
Luijma <72933633+Luijma@users.noreply.github.com>
2022-03-08 22:18:44 -0400

    Create 102-top_city.sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- displays top 3 of cities temperature during July and August, in descending orer by temp
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month BETWEEN 7 AND 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
