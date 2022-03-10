37e1e0cbe5bbf10c8e8abc91548374673cc4c3c5
Luijma <rsluismanuel@gmail.com>
2022-03-10 02:29:55 +0000

    trying join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,7 @@
 -- lists all cities contained in databaes hbtn_0d_usa
 SELECT
 	cities.id, cities.name, states.name
-FROM cities, states
-ORDER BY cities.id;
+FROM cities
+JOIN states
+ON cities.id = states.id
+ORDER BY cities.id ASC;
