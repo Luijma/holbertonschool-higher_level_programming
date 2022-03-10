b5795ad20a60a84ea4579336d9416258ea1db848
Luijma <rsluismanuel@gmail.com>
2022-03-10 01:29:45 +0000

    t6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates table hbtn_0d_usa with the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-	id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT NOT NULL UNIQUE AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY(id));
