7bcb5b0d37e4d94b159b733034e7b17ecb2a2fc6
Luijma <rsluismanuel@gmail.com>
2022-03-10 02:52:40 +0000

    fixed typo

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,4 +4,4 @@ SELECT
 FROM
 	tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY Tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
