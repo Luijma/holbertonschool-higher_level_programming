d1818b5c2073aab4c75f65ecc33fff6d2d4de397
Luijma <rsluismanuel@gmail.com>
2022-04-04 23:40:36 +0000

    Task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Prints message
+
+const argc = process.argv.length;
+
+if (argc === 2) {
+  console.log('No argument');
+} else if (argc === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
