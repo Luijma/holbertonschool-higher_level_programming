aa9b4df7d6118d06ab1ff0744ad79ef50b5d44a4
Luijma <rsluismanuel@gmail.com>
2022-04-05 00:21:03 +0000

    T9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints addition of two integers
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+const result = a + b;
+
+console.log(parseInt(result));
