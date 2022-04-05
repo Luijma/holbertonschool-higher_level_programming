e8f752130db5b988dda3011adbbb8c5560e536e7
Luijma <rsluismanuel@gmail.com>
2022-04-05 01:03:42 +0000

    t11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,12 @@
 #!/usr/bin/node
 // finds second biggest number in integers
-const args = process.argv;
+const args = process.argv.slice(2);
 const argc = process.argv.length;
-let biggest = args[2];
-let second = args[2];
-
-for (let i = 2; i < argc; i++) {
-  if (parseInt(args[i]) > parseInt(biggest)) {
-    second = biggest;
-    biggest = args[i];
-  }
-}
 
 if (argc <= 3) {
   console.log(0);
 } else {
+  args.sort((a, b) => { return b - a; });
+  const second = args[1];
   console.log(second);
 }
