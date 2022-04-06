de515351925399e79b9c77589781eceecd0853ec
Luijma <rsluismanuel@gmail.com>
2022-04-06 02:00:13 +0000

    t6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    super.print(c);
+  }
+}
+
+module.exports = Square;
