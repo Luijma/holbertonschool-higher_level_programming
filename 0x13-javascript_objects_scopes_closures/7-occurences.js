6eff6d55729a101a701d33344114feb9a7d90b50
Luijma <rsluismanuel@gmail.com>
2022-04-06 02:04:39 +0000

    t7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurences.js
@@ -0,0 +1,10 @@
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+
+  for (const element of list) {
+    if (element === searchElement) {
+      count++;
+    }
+  }
+  return count;
+}
