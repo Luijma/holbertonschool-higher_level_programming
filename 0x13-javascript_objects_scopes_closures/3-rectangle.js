56cfd24c3124a7032d51aa1a0b7905b02deed083
Luijma <rsluismanuel@gmail.com>
2022-04-06 01:26:28 +0000

    t3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        console.log('X');
+      }
+      console.log('\n');
+    }
+  }
+}
+module.exports = Rectangle;
