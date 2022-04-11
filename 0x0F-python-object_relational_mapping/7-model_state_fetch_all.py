dc28bd55a88833a485b27cb63a976e7bd5b01cdf
Luijma <rsluismanuel@gmail.com>
2022-04-11 06:00:09 +0000

    tasks until 8

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Lists all objects from the database """
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
+import sqlalchemy
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(engine)
+    with Session() as session:
+        statement = select(State).order_by(State.id)
+        states = session.execute(statement).scalars().all()
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class definition for State"""
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ Inherits from base class to link MYSQL Table state"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
