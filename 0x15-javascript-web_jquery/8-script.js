741a7082126525e9318ef220e57a495f0be69a56
Luijma <rsluismanuel@gmail.com>
2022-05-26 02:07:01 +0000

    t8 typo

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', (movies) => {
   for (const movie of movies.results) {
-    $('UL#list_movies').append('<li>' movie.title + '</li>');
+    $('UL#list_movies').append('<li>' + movie.title + '</li>');
   }
 });
